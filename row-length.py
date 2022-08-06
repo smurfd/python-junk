b166ef403b96cbd22d5adcea01b7b093327243f9
Nicklas Boman <smurfd@gmail.com>
2022-08-06 22:46:53 +0200

    Update row-length.py

--- row-length.py
+++ row-length.py
@@ -1,5 +1,2 @@
-inf = open("file.txt", 'r') 
-i = 0
-for line in inf.readlines():
-  i += 1
-  if len(line) < 81: print("row", i, "has the length", len(line))
+for i, line in enumerate(open("file.txt", 'r').readlines()): if len(line) < 81:
+  print("row", i, "has the length", len(line))
