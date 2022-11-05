2e116f6a9ed813e70951386152b5e5d0a3235be2
Nicklas Boman <smurfd@gmail.com>
2022-11-05 02:08:17 +0100

    Create biiiig_numbers.py

---
+++ biiiig_numbers.py
@@ -0,0 +1,4 @@
+import sys
+sys.set_int_max_str_digits(133713371) # enable bigger numbers, to avoid error : ValueError: Exceeds the limit (4300) for integer string conversion
+x = 131337**1313371
+print(x)
